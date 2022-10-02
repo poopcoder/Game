25c21e048fe0c3ab9b59d17d3a9cab96170b5d4d
VI269 <111111107+VI269@users.noreply.github.com>
2022-10-02 17:18:02 +0800

    changes

--- chat/code.py
+++ chat/code.py
@@ -1,9 +1,16 @@
 import json
+import random
 
 while True:
     inp = input("> ")
     ints = {}
     with open('intents.json', 'r') as f:
         json.dump(f, ints)
-    
-    print()
+    try:
+        if ints[inp].type() == list:
+            val = random.choice(ints[inp])
+        else:
+            val = ints[inp]
+        print(val)
+    except:
+        print("I don't understand.")
